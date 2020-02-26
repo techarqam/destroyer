93e6f4a411edb0a20d969bae8ae8ec83aac9e10b
arqam-rabbit <ramakant@rabbitdigital.in>
2020-02-26 20:26:28 +0530

    Working , Tested on linux

--- controller.js
+++ controller.js
@@ -2,8 +2,24 @@ const cmd = require('node-cmd');
 const chalk = require('chalk');
 
 exports.deleteAll = async function (req, res) {
-    await cmd.run("cls");
-    await cmd.run("node -v");
+
+    console.log(chalk.red('Starting Deletion'))
+    cmd.get(
+        `
+        cd ..
+        cd test
+        git init
+        `,
+        function(err, data, stderr){
+            if (!err) {
+               console.log(chalk.red('Deletion Completed',data))
+            } else {
+               console.log('error', err)
+            }
+ 
+        }
+    );
+
     await console.log(chalk.red("Deleted"))
     res.json({ status: 'Deleting' });
 
